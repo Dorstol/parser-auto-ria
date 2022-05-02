27748e9c7847386a2429de95e22067705be44615
Dorstol <olehsofronov@gmail.com>
2022-05-02 14:59:48 +0300

    fix filters

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import requests
 
 x = 0
 
-
-
 while True:
     if x == 0:
         url = 'https://auto.ria.com/car/used/'
@@ -16,13 +14,13 @@ while True:
     contents = soup.find_all('div', class_='content')
 
     for content in contents:
-        name = content.find('a', attrs={'class':'address'}).get('title')
-        if name.__contains__('BMW'):
-            sublink = content.find('a', attrs={'class': 'address'}).get('href')
-            price = content.find('div', attrs={'class': 'price-ticket'}).get('data-main-price')
+        name = content.find('a', attrs={'class': 'address'}).get('title')
+        sublink = content.find('a', attrs={'class': 'address'}).get('href')
+        price = content.find('div', attrs={'class': 'price-ticket'}).get('data-main-price')
+        if "Hyundai" in name and int(price) <= 7000:
             print(str(name) + ' - ' + str(price) + ' USD' + ' - ' + str(sublink))
         else:
-            break
+            continue
 
     btn_next = soup.find('a', class_='page-link js-next')
     if btn_next is not None:
